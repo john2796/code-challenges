749eaabe1017f993fcaa3eadf5ce6b3b7bc55362
john2796 <jbmiranda22796@gmail.com>
2019-11-24 11:50:03 -0800

    add char with 5 steps techqnique

--- Algos/add.js
+++ Algos/add.js
@@ -2,9 +2,17 @@
 //  write a function which takes two numberes and returns their sum.
 // ====================================
 
+// Step 1 Understand the Problem
 // 1. can i restate the problem in my own words ?
 //  - "Implement addition"
 // 2. What are the inputs that go into the problem
+// - ints?
+// - floats?
+// - what about string for large numbers ?
 // 3. What are the outputs that should come from the solution to the problem?
-// 4. Can the outputs be determined from the inputs ? In other words, do i have enough information to solve the problem ?
-// 5. How should I label the important pieces of data that are a part of the problem ?
+// - ints?
+// - floats?
+// - string?
+// 4. do i have enough information to solve the problem ?
+
+// 5. How should I label the important pieces of data that are a part of the problem ? -- what matters ?
---
+++ Algos/charCount.js
@@ -0,0 +1,87 @@
+/*
+========================================================================
+  Write a function which takes in a string and returns counts of each character in the string.
+ ========================================================================
+
+ ========================================================================
+ Step 1 Understand Problem
+ ========================================================================
+     1. restate problem on your own word
+             - Return count of each char 
+     2. input
+             - string
+     3. output 
+             - Number total of char
+     4. enough info to solve ? 
+             ???
+     5. How should I label the important pieces of data that are a part of the problem ? -- what matters ?
+            / ???
+
+
+========================================================================
+ Step 2 Explore Concrete Examples
+========================================================================
+
+     1. Simple Example
+         charCount('aaaa')  {a:4}
+         charCount('hello')  {h:1, e:1, l:2, o:1}
+        
+     2. Complex Example
+       charCount("My PIN number is 182213")
+              question to ask
+                - do we care for upper / lower cases 
+                - do we store white spaces 
+     3. Empty Inputs
+           charCount("")
+           what should i return : null, undefined , error ... etc
+     4. Invalid Inputs
+           ex. {}, [], number , string ???
+
+========================================================================
+Step 3 : Break it Down !
+========================================================================
+
+  function charCount(str) {
+          make object to return at end
+          loop over string, for each character...
+          if the char is a number/letter AND is a key in object, add one to count
+          if the char is a number/letter AND not in object, add it to object and set value to 1
+         if character is something else (space, period, etc) don't do anything
+          return object at end
+   }
+ charCount("aaaa") //{a:4}
+
+ */
+
+// ========================================================================
+// Step 4 : Solve / Simplify
+// ========================================================================
+
+function charCount(str) {
+  // make object to return at end
+  let obj = {}
+  // loop over string, for each character...
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i].toLowerCase()
+    // if the char is a number/letter AND is a key in object, add one to count
+    if (
+      typeof char === "number" ||
+      (typeof char === "string" && obj[char] > 0)
+    ) {
+      obj[char] += 1
+    }
+    // else the char is a number/letter AND not in object, add it to object and set value to 1
+    else {
+      obj[char] = 1
+    }
+    // if character is something else (space, period, etc) don't do anything
+  }
+  // return object at end
+  return obj
+}
+// charCount("aaaa") //{a:4}
+// charCount("hello") //  {h:1, e:1, l:2, o:1}
+// charCount("My PIN number is 182213")
+console.log(charCount("hello there!"))
+
+// Step 5: look Back and Refactor
