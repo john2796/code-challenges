4c3940b067eb3d60f880d5539e9cb5ec3f1168ac
john2796 <jbmiranda22796@gmail.com>
2019-12-04 15:22:37 -0800

    added sum range

--- recursion/lectureCC.js
+++ recursion/lectureCC.js
@@ -27,3 +27,21 @@ function isOdd(arr) {
 }
 
 // isOdd([4,4,8])
+
+function sumRange(num) {
+  if (num === 1) return 1
+  return num + sumRange(num - 1)
+}
+sumRange(4)
+/*
+sumRange(4)
+ 4 + sumRange(3)            (6+4) = 10 answer
+      3 + sumRange(2)       (3+3) = 6
+          2 + sumRange(1)   (2+1) = 3
+                     return 1
+
+sumRange(3)
+3 + sumRange(2)         (3+3) = 6
+  2 + sumRange(1)       (2+3)
+            return 1
+*/
