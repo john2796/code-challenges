725bb91cb077d75fe59214a7fd4aef6b62b25bc6
john2796 <jbmiranda22796@gmail.com>
2019-12-04 15:00:08 -0800

    added isOdd

--- recursion/lectureCC.js
+++ recursion/lectureCC.js
@@ -10,3 +10,20 @@ function countDown(num) {
   countDown(num)
 }
 // countDown(3)
+function isOdd(arr) {
+  // base
+  if (arr.length <= 0) {
+    console.log("All numbers are even")
+    return
+  }
+  // condition to check if it's odd
+  if (arr[arr.length - 1] % 2 !== 0) {
+    console.log(`${arr[arr.length - 1]} is odd`)
+    return
+  }
+  // different input
+  arr.pop()
+  isOdd(arr)
+}
+
+// isOdd([4,4,8])
