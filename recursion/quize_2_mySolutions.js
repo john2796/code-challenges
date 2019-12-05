a5560a4a55353bfc5dd656efdd5bd5a168df38ab
john2796 <jbmiranda22796@gmail.com>
2019-12-05 15:10:50 -0800

    completed cc 2

--- recursion/quize_2_mySolutions.js
+++ recursion/quize_2_mySolutions.js
@@ -60,3 +60,36 @@ someRecursive([1, 2, 3, 4], isOdd) // true
 // someRecursive([4,6,8,9], isOdd) // true
 // someRecursive([4,6,8], isOdd) // false
 // someRecursive([4,6,8], val => val > 10); // false
+
+/*
+ - write a recursive function called flatten,
+ - which accepts an array of arrays 
+ - and returns a new array with all values flattened.
+*/
+function flatten(a) {
+  if (a.length === 0) return []
+  let newArr = []
+  for (let val of a) {
+    console.log(Array.isArray(val))
+    // check if val is an array
+    if (Array.isArray(val)) {
+      // spread old vals , u can also use concat
+      newArr = [...newArr, ...flatten(val)]
+    } else {
+      newArr.push(val) // val is not arr , add it to newArr
+    }
+  }
+  return newArr
+}
+/* 
+ 1, 2, 3, [4, 5]
+ res = [1]
+ 1 is array ? no, add to res
+ 2...
+ 3...
+ [4,5] is array ? , yes ,then copy prev values and call itself with the array as new parameter
+*/
+flatten([1, 2, 3, [4, 5]]) // [1, 2, 3, 4, 5]
+// flatten([1, [2, [3, 4], [[5]]]]) // [1, 2, 3, 4, 5]
+// flatten([[1],[2],[3]]) // [1,2,3]
+// flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]) // [1,2,3
