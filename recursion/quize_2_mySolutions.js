399ae79dba92d313ff2e45522e1d63c3962f9aa0
john2796 <jbmiranda22796@gmail.com>
2019-12-05 14:55:07 -0800

    ..add my solution

--- recursion/quize_2_mySolutions.js
+++ recursion/quize_2_mySolutions.js
@@ -43,3 +43,20 @@ function isPalindrome(str) {
 // isPalindrome('tacocat') // true
 // isPalindrome('amanaplanacanalpanama') // true
 // isPalindrome('amanaplanacanalpandemonium') // false
+
+/*
+ someRecursive
+ - accepts an array and a callback
+ - the func returns true if a single value 
+ - in the array returns true when passed to the callback ,   else false
+*/
+function someRecursive(array, callback) {
+  if (array.length === 0) return false
+  if (callback(array[0])) return true
+  return someRecursive(array.slice(1), callback)
+}
+const isOdd = val => val % 2 !== 0
+someRecursive([1, 2, 3, 4], isOdd) // true
+// someRecursive([4,6,8,9], isOdd) // true
+// someRecursive([4,6,8], isOdd) // false
+// someRecursive([4,6,8], val => val > 10); // false
