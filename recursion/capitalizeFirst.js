2f53c81bd8b704d0e8e294aa0fc0a6383be62287
john2796 <jbmiranda22796@gmail.com>
2019-12-05 15:43:04 -0800

    added capitalizedFirst

---
+++ recursion/capitalizeFirst.js
@@ -0,0 +1,15 @@
+/*
+- Write a func called capitalizeFirst
+- Given an array of strings, 
+- capitalize the first letter of each string in the array
+*/
+
+function capitalizeFirst(arr) {
+  if (arr.length === 0) return []
+  let result = []
+  result.push(arr[0].charAt(0).toUpperCase() + arr[0].slice(1))
+  result = result.concat(capitalizeFirst(arr.slice(1)))
+  return result
+}
+
+capitalizeFirst(["car", "taco", "banana"]) // ['Car','Taco','Banana']
