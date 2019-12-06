9921ea4be28c6a04e03d14054cad7a0f0f54de0d
john2796 <jbmiranda22796@gmail.com>
2019-12-06 11:53:21 -0800

    linear

--- search-algo/search.js
+++ search-algo/search.js
@@ -1,24 +1,17 @@
-
-function search(arr, target){
-
-  let min = 0;
-  let max = arr.length - 1 
-
-  while(min <= max){
-    let middle = Math.floor((min + max ) / 2)
-    if(target === arr[middle]){
+function search(arr, target) {
+  let min = 0
+  let max = arr.length - 1
+  while (min <= max) {
+    let middle = Math.floor((min + max) / 2)
+    if (target === arr[middle]) {
       return middle
     } else if (arr[middle] < target) {
-      // 1, 2, 3, 4, 5, 6
-      //  3 < 4 NO the value has to on the right then , so move        pointers
-      min = middle + 1;
+      min = middle + 1
     } else {
-      max = middle -1;
+      max = middle - 1
     }
   }
-
-  return -1 
+  return -1
 }
 
-
-search([1,2,3,4,5,6] , 1)
+search([1, 2, 3, 4, 5, 6], 1)
