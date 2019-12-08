a9a040f57b1aa213c4fd7c0dcb7029b4ca9009b9
john2796 <jbmiranda22796@gmail.com>
2019-12-07 18:22:56 -0800

    sort by length

--- sorting/index.js
+++ sorting/index.js
@@ -3,3 +3,10 @@ function sort(arr) {
 }
 // sort ascending order
 sort([23, 45, 6, 12, 13])
+
+// sort by length
+function sortByLength(arr) {
+  return arr.sort((a, b) => a.length > b.length)
+}
+// sort ascending order
+sortByLength(["miiko", "pogi", "is ", "in ", "the", "house"])
