f2af56f01c35258207310459a7e6189638976217
john2796 <jbmiranda22796@gmail.com>
2019-12-08 10:35:23 -0800

    naiveSearchString

--- sorting/index.js
+++ sorting/index.js
@@ -1,12 +1,9 @@
 function sort(arr) {
   return arr.sort((a, b) => a > b)
 }
-// sort ascending order
-sort([23, 45, 6, 12, 13])
+sort([23, 45, 6, 12, 13]) // sort ascending order
 
-// sort by length
 function sortByLength(arr) {
   return arr.sort((a, b) => a.length > b.length)
 }
-// sort ascending order
-sortByLength(["miiko", "pogi", "is ", "in ", "the", "house"])
+sortByLength(["miiko", "pogi", "is ", "in ", "the", "house"]) // sort by length
