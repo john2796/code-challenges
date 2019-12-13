c678cb247aedc1bab9ee3167e1df3dd6ab75e245
john2796 <jbmiranda22796@gmail.com>
2019-12-12 19:39:32 -0800

    merge function

--- sorting/index.js
+++ sorting/index.js
@@ -80,3 +80,34 @@ function insertionSort(arr) {
 */
 
 insertionSort([2, 1, 9, 76, 4])
+/* Merging Arrays Pseudocode 
+- create an empty array , take a look at the smallest values in each input array.
+- while there are still values we haven't looked at...
+   - if the value in the first array is smaller than the value in the second array , push the value in the first arry into the second array, push the value in the first array into our results and move on to the next value in the second array
+   - Once we exhaust one array, push in all remaining values from the other array
+*/
+function merge(arr1, arr2) {
+  let results = []
+  let p1 = 0
+  let p2 = 0
+  while (p1 < arr1.length && p2 < arr2.length) {
+    if (arr2[p2] > arr1[p1]) {
+      results.push(arr1[p1])
+      p1++
+    } else {
+      results.push(arr2[p2])
+      p2++
+    }
+  }
+
+  while (p1 < arr1.length) {
+    results.push(arr1[p1])
+    p1++
+  }
+  while (p2 < arr2.length) {
+    results.push(arr2[p2])
+    p2++
+  }
+  return results
+}
+merge([1, 10, 50], [2, 14, 100])
