e4963b7bace9150666a09327272f2a284613190a
john2796 <jbmiranda22796@gmail.com>
2019-12-16 13:47:17 -0800

    added couple more methods

--- dsta-structures/linkedLists.js
+++ dsta-structures/linkedLists.js
@@ -4,14 +4,12 @@ class Node {
     this.next = null
   }
 }
-
 class SinglyLinkedLists {
   constructor() {
     this.head = null
     this.tail = null
-    this.length = null
+    this.length = 0
   }
-
   push(val) {
     let newNode = new Node(val)
 
@@ -25,8 +23,40 @@ class SinglyLinkedLists {
     this.length++
     return this
   }
-
-  pop() {}
+  traverse() {
+    let current = this.head
+    while (current) {
+      console.log(current.val)
+      current = current.next
+    }
+  }
+  pop() {
+    if (!this.head) return undefined
+    let current = this.head
+    let prev = current
+    while (current.next) {
+      prev = current
+      current = current.next
+    }
+    this.tail = prev
+    this.tail.next = null
+    this.length--
+    if (this.length === 0) {
+      this.head = null
+      this.tail = null
+    }
+    return current
+  }
+  shift() {
+    if (!this.head) return undefined
+    let current = this.head
+    this.head = current.next
+    this.length--
+    return current
+  }
 }
 
 let list = new SinglyLinkedLists()
+list.push("one")
+list.push("two")
+list.push("three")
