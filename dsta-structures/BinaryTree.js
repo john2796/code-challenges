3841585e63e12ddc672ff8b413cea0eca9d11617
john2796 <jbmiranda22796@gmail.com>
2019-12-20 18:22:46 -0800

    added BFS

--- dsta-structures/BinaryTree.js
+++ dsta-structures/BinaryTree.js
@@ -18,7 +18,7 @@ class BinarySearchTree {
       return this
     }
     let current = this.root
-    while (true) {
+    while (current) {
       if (value < current.value) {
         if (current.left === null) {
           current.left = newNode
@@ -55,30 +55,36 @@ class BinarySearchTree {
     if (!this.root) return false
     let current = this.root
     while (current) {
-      if (value === current.value) return true
+      if (value === current.value) return current
       if (value < current.value) {
         current = current.left
       } else {
         current = current.right
       }
-      //  if(value < current.value){
-      //    current = current.left
-      //  } else if (value > current.value){
-      //    current = current.right
-      //  } else {
-      //    return current
-      //  }
     }
     return false
+  }
+
+  // Breadth First Search
+  bfs(value) {
+    let queue = []
+    let data = []
+    let current = this.root
+    queue.push(current)
+    while (queue.length) {
+      current = queue.shift()
+      data.push(current.value)
+      if (current.left) queue.push(current.left)
+      if (current.right) queue.push(current.right)
+    }
+    return data
   }
 } // end of bst
 
 var t = new BinarySearchTree()
 t.insert(10)
-t.insert(5)
-t.insert(13)
-
-// t.insert(2)
-// t.insert(7)
-// t.insert(11)
-// t.insert(16 )
+t.insert(6)
+t.insert(15)
+t.insert(3)
+t.insert(8)
+t.insert(20)
