69c3d56fc02872715c49c69ccef417a1a087f874
john2796 <jbmiranda22796@gmail.com>
2020-02-19 22:37:36 -0800

    added description

--- leetcode/day-2.js
+++ leetcode/day-2.js
@@ -11,6 +11,17 @@
  
  
     Keywords: max depth, longest path from the root nodoe down the farthest leaf node
+
+
+Given a binary tree, find its maximum depth.
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Given binary tree [3,9,20,null,null,15,7],
+solved: X
  */
 var maxDepth = function(root) {
   if (root === null) {
