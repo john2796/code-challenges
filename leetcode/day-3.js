f5ac5242a80fd87ca10511c4f0310da90b7e5594
John Benedict Miranda <jbmiranda22796@gmail.com>
2020-02-20 23:49:00 -0800

    Added Another challenge on day 3 out of 365 challenge

--- leetcode/day-3.js
+++ leetcode/day-3.js
@@ -31,3 +31,20 @@ var moveZeroes = function(nums) {
   }
   return nums
 }
+
+
+//==================================== 
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    for(let i = 0 ; i < nums.length;i++){
+        if(nums[i] === val){
+            nums.splice(i,1)
+            i--
+        }
+    }
+    return nums.length
+};
