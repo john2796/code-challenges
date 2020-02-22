be8d12382171802a4d375d9880956df562f978f3
john2796 <jbmiranda22796@gmail.com>
2020-02-21 18:57:30 -0800

    completed strStr day 4 out 365 challenge

---
+++ leetcode/day-4.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ * 
+ * 
+ * Implement strStr().
+
+Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+Example 1:
+
+Input: haystack = "hello", needle = "ll"
+Output: 2
+ */
+var strStr = function(haystack, needle) {
+  return haystack.indexOf(needle)
+}
