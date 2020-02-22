d4e2500a7cbad2a8dabf4d23a80b3fb58c62d01c
john2796 <jbmiranda22796@gmail.com>
2020-02-21 21:14:09 -0800

    searchInsert completed day 4

--- leetcode/day-4.js
+++ leetcode/day-4.js
@@ -16,3 +16,22 @@ Output: 2
 var strStr = function(haystack, needle) {
   return haystack.indexOf(needle)
 }
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+  if (target === 0) return 0
+
+  for (let i = 0; i < nums.length; i++) {
+    let number = nums[i]
+    if (number >= target) {
+      return i
+      break
+    } else if (i + 1 == nums.length) {
+      return i + 1
+    }
+  }
+}
