7a7445c612fa7fa4f1b053f95c9d58ec89330f7a
john2796 <jbmiranda22796@gmail.com>
2020-03-03 13:22:41 -0800

    rangeSumBST completed day 14

--- leetcode/day-14.js
+++ leetcode/day-14.js
@@ -29,3 +29,49 @@ var findNumbers = function(nums) {
   }
   return count
 }
+
+/**
+ * Given the root node of a binary search tree, return the sum of values of all nodes with value between L and R (inclusive).
+
+The binary search tree is guaranteed to have unique values.
+
+ 
+
+Example 1:
+
+Input: root = [10,5,15,3,7,null,18], L = 7, R = 15
+Output: 32
+Example 2:
+
+Input: root = [10,5,15,3,7,13,18,1,null,6], L = 6, R = 10
+Output: 23
+ 
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} L
+ * @param {number} R
+ * @return {number}
+ 
+
+                10
+            5.      15
+        3      7        18
+
+ */
+var rangeSumBST = function(root, L, R) {
+  let data = []
+  function traverse(node) {
+    if (node.left) traverse(node.left)
+    data.push(node.val)
+    if (node.right) traverse(node.right)
+  }
+  traverse(root)
+
+  return data.filter(x => x >= L && x <= R).reduce((a, b) => a + b)
+}
